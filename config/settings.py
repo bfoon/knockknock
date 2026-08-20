d2ecceb5acc829292fe394deb0cc5d258f424204
bfoon <baboucarrfoon@gmail.com>
2026-08-20 14:25:19 +0000

    start46

--- config/settings.py
+++ config/settings.py
@@ -136,7 +136,7 @@ USE_TZ = True
 
 # Static / media
 STATIC_URL = "static/"
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = "/var/www/nokknock/staticfiles"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
